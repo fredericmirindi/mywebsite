2fbcc3e0063915856a085174706585848b30511b
fredericmirindi <56483530+fredericmirindi@users.noreply.github.com>
2024-06-27 18:36:32 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const links = document.querySelectorAll('nav ul li a');
-
-    for (const link of links) {
-        link.addEventListener('click', smoothScroll);
-    }
-
-    function smoothScroll(event) {
-        event.preventDefault();
-        const targetId = event.currentTarget.getAttribute('href').substring(1);
-        const targetSection = document.getElementById(targetId);
-
-        targetSection.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    }
+document.addEventListener("DOMContentLoaded", function() {
+    // JavaScript code goes here
 });
