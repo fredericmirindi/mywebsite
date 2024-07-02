9e40b5291c26aac8f5c10aea33d992bf23205e28
fredericmirindi <56483530+fredericmirindi@users.noreply.github.com>
2024-07-01 21:24:21 -0500

    Deploying website files

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const devInlineBackends = [
+    // add whatever here if needed
+];
+
+module.exports = {
+    reactStrictAssets: true,
+    estStrict: false,
+    devInlineBackends: devInlineBackends
+};
