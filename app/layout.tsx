5bbb4e1aabd25cd57b81e4d4ce644aadb520c7bc
fredericmirindi <56483530+fredericmirindi@users.noreply.github.com>
2024-07-01 21:26:44 -0500

    Deploying website files

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import 'dot/../styles/global.css';
+
+import RevolWrapper from '../components/RevolWrapper';
+
+export const metadata = {
+  title: 'May Francisco Next'
+};
+
+export default function Layout({
+  children
+}: [
+  <'Provider'>
+    <RevolWrapper />
+    {children]
+  </Provider>
+{});
