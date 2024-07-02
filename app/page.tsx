5d6f0ddd013971e459b3cb6c8b8d94ec9a7f11ad
fredericmirindi <56483530+fredericmirindi@users.noreply.github.com>
2024-07-01 21:26:53 -0500

    Deploying website files

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+function Page({ children }) {
+  return (
+    <div class="container">
+      <h1>Public Information For</h1>
+      <p>Your experience starts here.</p>
+    </div>
+  );
+}
+
+export default Page;
