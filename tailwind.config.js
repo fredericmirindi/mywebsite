405a6be5d7924f447adf61bb579bd6e2e9946b2c
fredericmirindi <56483530+fredericmirindi@users.noreply.github.com>
2024-07-01 21:25:55 -0500

    Deploying website files

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    content: {
+          hovers,: ['' ]
+         },
+        folder: ['all(sql)', 'name(header', 'top', 'bottom', 'menu', 'tailed', 'body'],
+        require: (require('postcss-import')(), () => require('tailwindcss/con') )
+    }
